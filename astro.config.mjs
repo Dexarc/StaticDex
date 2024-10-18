650a9b7173dc949be15480997fe66baeb10a2b71
Dexarc <sarenkumar86@gmail.com>
2024-10-18 15:04:47 +0530

    href change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,6 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({});
+export default defineConfig({
+    integrations: [image()],
+    site: 'https://StaticDex.github.io',
+    base: '/StaticDex'  
+});
